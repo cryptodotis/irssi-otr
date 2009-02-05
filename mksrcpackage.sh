fdb0178791aba2cf0d106ac69b7736194ea08214
Uli Meis <a.sporto+bee@gmail.com>
2009-02-05 11:11:27 +0100

    Added a third tarball irc-otr for both plugins.

--- mksrcpackage.sh
+++ mksrcpackage.sh
@@ -3,6 +3,18 @@ if [ -z "$2" ]; then echo "Syntax: $0 <src-dir> <git-commit/tag>"; exit 1;fi
 SDIR="$1"
 #VER=`(cd "$SDIR/.git/refs/tags/" && ls -t)|head -n1|sed -e 's/.//'`
 VER=$2
+
+PKG=irc-otr-$VER.tar
+HDIR=irc-otr-$VER
+mkdir "$HDIR" &&\
+(cd "$SDIR" && git archive --format=tar --prefix=irc-otr-$VER/ HEAD )>$PKG &&\
+(cd "$HDIR" && ln -s ../irssi-headers &&\
+	echo -e "SET(IRSSIOTR_VERSION $VER)" >tarballdefs.cmake) &&\
+tar rhf $PKG "$HDIR" &&\
+rm $HDIR/{irssi-headers,tarballdefs.cmake} &&\
+rmdir $HDIR &&\
+gzip $PKG
+
 PKG=irssi-otr-$VER.tar
 HDIR=irssi-otr-$VER
 mkdir "$HDIR" &&\
@@ -13,7 +25,6 @@ tar rhf $PKG "$HDIR" &&\
 rm $HDIR/{irssi-headers,tarballdefs.cmake} &&\
 rmdir $HDIR &&\
 gzip $PKG
-
 
 PKG=xchat-otr-$VER.tar
 HDIR=xchat-otr-$VER
