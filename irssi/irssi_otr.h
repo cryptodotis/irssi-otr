9ea5cc4e2e41753ee170bfd3738a4ab66311121e
David Goulet <dgoulet@ev0ke.net>
2012-09-13 17:21:21 -0400

    Standardize coding style in irssi header
    No behavior changed.
    
    Signed-off-by: David Goulet <dgoulet@ev0ke.net>
    [ulim: rebased on HEAD]
    
    http://irc-otr.tuxfamily.org/bugs/show_bug.cgi?id=2

--- irssi/irssi_otr.h
+++ irssi/irssi_otr.h
@@ -1,21 +1,25 @@
 /*
  * Off-the-Record Messaging (OTR) module for the irssi IRC client
+ *
  * Copyright (C) 2008  Uli Meis <a.sporto+bee@gmail.com>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,USA
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301,USA
  */
+
+#ifndef IRSSI_OTR_H
+#define IRSSI_OTR_H
 
 #include <common.h>
 #include <core/commands.h>
@@ -39,24 +43,6 @@
 #define IRC_CTX SERVER_REC
 
 #define get_client_config_dir get_irssi_dir
-
-static IRC_CTX *IRCCTX_DUP(IRC_CTX *ircctx) __attribute__ ((unused));
-
-static IRC_CTX *IRCCTX_DUP(IRC_CTX *ircctx)
-{
-	server_ref(ircctx);
-	return ircctx;
-}
-
-static IRC_CTX *IRCCTX_FREE(IRC_CTX *ircctx) __attribute__ ((unused));
-
-static IRC_CTX *IRCCTX_FREE(IRC_CTX *ircctx)
-{
-	server_unref(ircctx);
-	return ircctx;
-}
-
-void otr_query_create(IRC_CTX *ircctx, const char *nick);
 
 #define IRCCTX_ADDR(ircctx) ircctx->connrec->address
 #define IRCCTX_NICK(ircctx) ircctx->nick
@@ -87,3 +73,19 @@ void otr_query_create(IRC_CTX *ircctx, const char *nick);
 			otr_query_create(ircctx, nick);	\
 			printformat(ircctx, nick, MSGLEVEL_MSGS, formatnum, \
 				    ## __VA_ARGS__); } }
+
+static inline IRC_CTX *IRCCTX_DUP(IRC_CTX *ircctx)
+{
+	server_ref(ircctx);
+	return ircctx;
+}
+
+static inline IRC_CTX *IRCCTX_FREE(IRC_CTX *ircctx)
+{
+	server_unref(ircctx);
+	return ircctx;
+}
+
+void otr_query_create(IRC_CTX *ircctx, const char *nick);
+
+#endif /* IRSSI_OTR_H */
