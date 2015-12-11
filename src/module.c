7592efe844aff10ce11443348f9e0d9307a2c324
Mike Quin <mike@elite.uk.com>
2015-12-11 21:17:34 +0000

    Fix error on self-messages with irssi versions after 0.8.17
    This commit fixes errors produced when the user atempts to send a mesasge to themselves,
    caused by the he addition of a target parameter to the 'message private' signal in
    https://github.com/irssi/irssi/commit/1edfcedda1105d79fa6b92d77a8fb60f296abe1e

--- src/module.c
+++ src/module.c
@@ -32,6 +32,7 @@
 #include "otr.h"
 #include "otr-formats.h"
 #include "utils.h"
+#include "irssi-version.h"
 
 GCRY_THREAD_OPTION_PTHREAD_IMPL;
 
@@ -90,8 +91,13 @@ end:
 /*
  * Pipes all incoming private messages through OTR
  */
+#if IRSSI_VERSION_DATE > 20141206
 void sig_message_private(SERVER_REC *server, const char *msg,
-		const char *nick, const char *address)
+		const char *nick, const char *address, const char *target)
+#else
+void sig_message_private(SERVER_REC *server, const char *msg,
+                const char *nick, const char *address)
+#endif
 {
 	int ret;
 	char *new_msg = NULL;
@@ -106,7 +112,11 @@ void sig_message_private(SERVER_REC *server, const char *msg,
 
 	if (!new_msg) {
 		/* This message was not OTR */
-		signal_continue(4, server, msg, nick, address);
+#if IRSSI_VERSION_DATE > 20141206
+		signal_continue(5, server, msg, nick, address, target);
+#else
+                signal_continue(4, server, msg, nick, address);
+#endif
 	} else {
 		/*
 		 * Check for /me IRC marker and if so, handle it so the user does not
